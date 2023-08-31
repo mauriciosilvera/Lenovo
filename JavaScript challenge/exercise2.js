8e28d19326aca83c43e802388ba8fb51660702af
mauriciosilvera <mauricioagustin_@outlook.com>
2023-08-31 10:27:31 -0300

    Improved javascript challenge functions, fixed import of banner.

--- JavaScript challenge/exercise2.js
+++ JavaScript challenge/exercise2.js
@@ -10,4 +10,19 @@ function isPalindrome(str) {
   return cleanStr === reversedStr;
 }
 
-console.log(isPalindrome('5Level5'));
+function isPalindromeWithoutMethods(str) {
+  let forwards = '';
+  let backwards = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    forwards += str[i].toLowerCase();
+  }
+
+  for (let j = str.length - 1; j >= 0; j -= 1) {
+    backwards += str[j].toLowerCase();
+  }
+
+  return forwards === backwards;
+}
+
+console.log(isPalindromeWithoutMethods('Refer'));
--- JavaScript challenge/exercise3.js
+++ JavaScript challenge/exercise3.js
@@ -1,21 +1,25 @@
 // 3) Create a function that receives two numbers and shows the prime numbers between them (it includes both number)
 
+function isPrime(num) {
+  // 0 and 1 are not prime numbers by definition.
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function primeNumbersBetween(num1, num2) {
   const result = [];
 
   for (let i = num1; i <= num2; i += 1) {
-    // Negative numbers, 0 and 1 are not prime numbers.
-    if (i <= 1) {
-      i += 1;
-    }
-
-    // 2, 3, 5 and 7 are prime numbers because they are divisible by 1 and itself. These are an exception to the next rule.
-    if (i === 2 || i === 3 || i === 5 || i === 7) {
-      result.push(i);
-    }
-
-    // At this point (number > 7), we assume every number is divisible by 1 and itself, so we need to checkout if it is divisible by another one.
-    if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
+    if (isPrime(i)) {
       result.push(i);
     }
   }
@@ -23,4 +27,4 @@ function primeNumbersBetween(num1, num2) {
   return result.join();
 }
 
-console.log(primeNumbersBetween(1, 37));
+console.log(primeNumbersBetween(1, 122));
